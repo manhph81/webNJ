7e93059d43073a2da50b5abc72d9bbd7e49165c1
manhph81 <manhph81@users.noreply.github.com>
2021-01-18 13:37:38 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://manh:admin@cluster0.kn2tv.mongodb.net/test', {
+        await mongoose.connect('mongodb+srv://admin:pN7aKAYO8rolidy9@cluster0.kn2tv.mongodb.net/test?authSource=admin&replicaSet=atlas-1370jf-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=truet', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
