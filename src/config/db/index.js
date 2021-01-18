47bd7e1cbc50547e539cbba9414dc7065493fcca
manhph81 <manhph81@users.noreply.github.com>
2021-01-18 13:56:30 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const connectString = 'mongodb+srv://admin:admin@cluster0.kn2tv.mongodb.net/blocktest'
 async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://admin:admin@cluster0.kn2tv.mongodb.net/blocktest', {
+        await mongoose.connect(connectString, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
